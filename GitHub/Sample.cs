57a24ef4a77eb891a837ba3e7670610159c7bb67
maheshrenikindi <maheshrenikindi.vtalent@gmail.com>
2019-05-01 13:27:53 +0530

    one method addded

--- GitHub/Sample.cs
+++ GitHub/Sample.cs
@@ -8,5 +8,13 @@ namespace GitHub
 {
     class Sample
     {
+        public int add()
+        {
+            int x = 10;
+            int y = 20;
+            int sum = x + y;
+            return sum;
+        }
+      
     }
 }
